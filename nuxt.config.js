f47d9ac6a6da606b44523524a060eefa76b154e4
NDQ <endyquang@gmail.com>
2020-06-08 21:32:56 +0700

    init project

---
+++ nuxt.config.js
@@ -0,0 +1,79 @@
+
+const title = 'Quang ND – Fullstack web developer'
+const description = 'A little about myself and my work. If you are interested, contact me to know more'
+
+export default {
+  mode: 'spa',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: description },
+
+      { hid: 'og:site_name', property: 'og:site_name', content: title },
+      { hid: 'og:title', property: 'og:title', content: title },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      // { hid: 'og:image', property: 'og:image', content: DEFAULT_SUMMARY_IMG },
+      { hid: 'og:description', property: 'og:description', content: description },
+
+      { hid: 'twitter:title', name: 'twitter:title', content: title },
+      // { hid: 'twitter:image', name: 'twitter:image', content: DEFAULT_SUMMARY_IMG },
+      { hid: 'twitter:description', name: 'twitter:description', content: description }
+      // { hid: 'twiiter:card', name: 'twitter:card', content: 'summary_large_image' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'href="https://fonts.googleapis.com/css2?family=Anonymous+Pro&display=swap' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@fortawesome/fontawesome-svg-core/styles.css',
+    '@/assets/css/main.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/fa',
+    '@/plugins/transition'
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  },
+
+  server: {
+    port: 9000
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      backgroundColor: {
+        layout: '#1d1d1d',
+        aside: '#242425',
+        popup: 'rgba(0,0,0,.8)',
+        'aside--hover': '#2e2e32',
+        'aside--active': '#3e3e45'
+      }
+    },
+    fontFamily: {
+      sans: ['Anonymous Pro', 'monospace']
+    }
+  },
+  variants: {},
+  plugins: []
+}

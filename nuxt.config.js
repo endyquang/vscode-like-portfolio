04f397943d0f28df0c492d5543c7ead4b7a21ed9
NDQ <endyquang@gmail.com>
2020-06-09 18:19:29 +0700

    allow scale on mobile

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
     title,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=0' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: description },
 
       { hid: 'og:site_name', property: 'og:site_name', content: title },
